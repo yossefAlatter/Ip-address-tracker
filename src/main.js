9819a1d75892bc36ffb495e881c1265418d1282b
yossefAlatter <yossefalatter@gmail.com>
2020-12-22 04:48:31 +0200

    first-commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//==import jquery==//
+import 'jquery';
+
+//==import bootstrap==//
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//==import css file==//
+import './css/style.css';
+
 Vue.config.productionTip = false
 
 new Vue({
